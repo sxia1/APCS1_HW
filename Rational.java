9b5eb846319c5b5051dd9bc3cf45e89e559d25be
sxia1 <sxia1@stuy.edu>
2017-11-20 07:37:07 -0500

    change

--- Rational.java
+++ Rational.java
@@ -6,46 +6,45 @@
 public class Rational{
 
     //INSTANCE VARS
-    private int numer;
-    private int denom;
+    //convention says use underscore
+    private int _numer;
+    private int _denom;
 
     //DEFAULT CONS
     public Rational(){
-	numer = 0;
-	denom = 1;
+	_numer = 0;
+	_denom = 1;
     }
 
     //CONS
     public Rational(int a, int b){
-	if (b == 0){
-	    //pretty sure there is a way to call the default cons...
-	    //whelp I forgot :P
-	    //For now this will do until I remember. I mean... it works
-	    numer = 0;
-	    denom = 1;
-	    System.out.println("Invalid inputs");
+	//to call default cons use this(), because it must be the first line make a conditional using b != 0 instead of b == 1
+	this();
+	if (b != 0){
+	    _numer = a;
+	    _denom = b;
 	}
 	else {
-	    numer = a;
-	    denom = b;
+	    System.out.println("Invalid inputs");
 	}
     }
 
     //TOSTRING
     public String toString(){
-	return numer + "/" + denom;
+	return _numer + " / " + _denom;
     }
 
     //floatValue
     public float floatValue(){
 	/*
-	 *QAF gave me the idea to mult by 0.1
-	 *without 1.0 * , the result is always 0
+	 *QAF gave me the idea to mult by 1.0
+	 *without 1.0 * , the result is always 0.0
 	 *must have to do with int division truncating
 	 *then it said double to float problem?
 	 *so I typecasted
+	 *typecasting takes priority over division
 	 */
-	return (float)(1.0 * numer/denom);
+	return (double)_numer/_denom;
     }
 
     //multiply
@@ -54,32 +53,67 @@ public class Rational{
 	//so I figured that I should set the instance vars instead
 	//is there a way to call the object Rational?
 	//I think my language is a little iffy there though...
-	numer = (numer * a.numer);
-	denom =	(denom * a.denom);
+	_numer = (_numer * a._numer);
+	_denom = (_denom * a._denom);
     }
     
     //divide
     public void divide(Rational a){
 	//to divide fractions, actually you can multiply :)
 	//so same as multiply but apply keep, change, flip
-	numer = (numer * a.denom);
-	denom =	(denom * a.numer);
+	_numer = (_numer * a._denom);
+	_denom = (_denom * a._numer);
     }
+    //=======================================================================
+    //MAX(a b)
+    public static int max(int a, int b){
+	if (a > b){
+	    return (a);
+	}
+	return (b);
+    }
+    public static double max(double a, double b){
+	if (a > b){
+	    return (a);
+	}
+	return (b);
+    }
+    
+    //add
+    //subtract
+    //gcd
+    //Returns the gcd of the numerator and denominator
+    //Uses Euclid's algorithm (reuse your old code!)
+    //Recall that in order for Euclid's algorithm to work, the first number must be greater than the second
+    public static void gcd(int a, int b){
+	
+    }
+    //reduce
+    //compareTo
+    
     
     //MAIN METHOD
     public static void main(String [] args){
 	Rational r = new Rational(2, 3);
 	Rational s = new Rational(1, 2);
-	System.out.println(r);// 2/3
-	System.out.println(s);// 1/2
-	System.out.println( r.floatValue() );//0.6666667
-	System.out.println( s.floatValue() );//0.5
+
+	//testing toString
+	System.out.println(r);// 2 / 3
+	System.out.println(s);// 1 / 2
+
+	//testing floatValue
+	System.out.println( r.floatValue() );// 0.6666667
+	System.out.println( s.floatValue() );// 0.5
+
+	//testing multiply
 	r.multiply(s);
-	System.out.println(r);// 2/6
-	System.out.println(s);// 1/2
+	System.out.println(r);// 2 / 6
+	System.out.println(s);// 1 / 2
+
+	//testing divide
 	r.divide(s);
-	System.out.println(r);// 4/6
-	System.out.println(s);// 1/2
-	//works however, does not return a simplified version...
+	System.out.println(r);// 4 / 6
+	System.out.println(s);// 1 / 2
+	
     }
 }
