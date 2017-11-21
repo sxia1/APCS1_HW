d42a2204821ace10383a45fdc76f24e58d210a8a
unknown <sxia1@stuy.edu>
2017-11-20 20:29:08 -0500

    Rational Fix and Update

--- Rational.java
+++ Rational.java
@@ -1,12 +1,12 @@
 //Sophia Xia
 //APCS1 pd1
-//HW36 -- Be Rational
-//2017-11-17
+//HW37 -- Be More Rational
+//2017-11-20
 
 public class Rational{
 
     //INSTANCE VARS
-    //convention says use underscore
+    //convention: use underscore
     private int _numer;
     private int _denom;
 
@@ -16,7 +16,7 @@ public class Rational{
 	_denom = 1;
     }
 
-    //CONS
+    //OVERLOADED CONS
     public Rational(int a, int b){
 	//to call default cons use this(), because it must be the first line make a conditional using b != 0 instead of b == 1
 	this();
@@ -40,11 +40,9 @@ public class Rational{
 	 *QAF gave me the idea to mult by 1.0
 	 *without 1.0 * , the result is always 0.0
 	 *must have to do with int division truncating
-	 *then it said double to float problem?
-	 *so I typecasted
 	 *typecasting takes priority over division
 	 */
-	return (double)_numer/_denom;
+	return (float)_numer/_denom;
     }
 
     //multiply
@@ -64,33 +62,62 @@ public class Rational{
 	_numer = (_numer * a._denom);
 	_denom = (_denom * a._numer);
     }
-    //=======================================================================
-    //MAX(a b)
-    public static int max(int a, int b){
-	if (a > b){
-	    return (a);
-	}
-	return (b);
-    }
-    public static double max(double a, double b){
-	if (a > b){
-	    return (a);
-	}
-	return (b);
-    }
+    
+    //=========================================================================
     
     //add
+    //writing this out on paper with variables instead of numbers helped a lot
+    //using numerical examples just confused me more XD
+    public void add(Rational a){
+	_numer = (_numer * a._denom) + (a._numer * _denom);
+	_denom = (_denom * a._denom);
+    }
+    
     //subtract
-    //gcd
+    //copied from add but switched + to -
+    public void subtract(Rational a){
+	_numer = (_numer * a._denom) - (a._numer * _denom);
+	_denom = (_denom * a._denom);
+    }
+    
+    //STATIC gcd
     //Returns the gcd of the numerator and denominator
     //Uses Euclid's algorithm (reuse your old code!)
     //Recall that in order for Euclid's algorithm to work, the first number must be greater than the second
-    public static void gcd(int a, int b){
-	
+    public static int gcd(int a, int b){
+	//note to self: this recursive method takes care of the max min problem
+	//rmbr? you had to explain it? in front of the class? and you traced it?
+	if (a % b == 0){
+	    return b;}
+	return gcd (b, a%b);
     }
-    //reduce
-    //compareTo
     
+    //gcd
+    //idea came from QAF, less coding :)
+    public int gcd(){
+	return gcd(_numer, _denom);
+    }
+
+    
+    //reduce
+    public void reduce(){
+	int holdgcd = gcd(_numer, _denom);
+	_numer = _numer / holdgcd;
+	_denom = _denom / holdgcd;
+    }
+    
+    //compareTo
+    public int compareTo(Rational a){
+	if (this.floatValue() == a.floatValue()){
+	    return 0;
+	}
+	else if (this.floatValue() > a.floatValue()){
+	    return 1;
+	}
+	else{
+	    return -1;
+	}
+    }
     
     //MAIN METHOD
     public static void main(String [] args){
@@ -114,6 +141,26 @@ public class Rational{
 	r.divide(s);
 	System.out.println(r);// 4 / 6
 	System.out.println(s);// 1 / 2
+
+	//testing gcd
+	System.out.println(r.gcd());//2
+	System.out.println(s.gcd());//1
 	
+	//testing reduce
+	r.reduce();
+	System.out.println(r);// 2 / 3
+	s.reduce();
+	System.out.println(s);// 1 / 2
+
+	//testing add/subtract
+	//note: these methods do not reduce the fraction
+	r.add(s);
+	System.out.println(r);// 7 / 6
+	r.subtract(s);
+	System.out.println(r);// 8 / 12
+
+	//testing compareTo
+	System.out.println( r.compareTo(s) ); // 1
+	System.out.println( s.compareTo(r) ); //-1
     }
 }
