aa991a49b1a85e5d731fd5631c1aed157331c516
unknown <sxia1@stuy.edu>
2017-11-23 18:10:04 -0500

    Swapper V2

--- Swapper.java
+++ Swapper.java
@@ -3,7 +3,18 @@
 //HW38 -- Put it Together
 //2017-11-21
 
-/* First prints out the array. Then prompts the user for inputs 4 times. It should be the row and column for the first String, then the row and column for the second string. After that the two Strings will swap places and the array will be printed out again.
+/*=======================================================================
+Version 1:
+First prints out the array. Then prompts the user for inputs 4 times.
+It should be the row and column for the first String, then the row and
+column for the second string. After that the two Strings will swap
+places and the array will be printed out again.
+=======================================================================*/
+
+/* Version 2:
+The columns and rows of the array are now labeled to make the program
+more user friendly.
+The user can now choose to continue swapping.
  */
 
 import cs1.Keyboard;
@@ -11,10 +22,10 @@ import cs1.Keyboard;
 public class Swapper{
 
     //InstVars
-    private static int a;
-    private static int b;
-    private static int c;
-    private static int d;
+    private static int row1;
+    private static int col1;
+    private static int row2;
+    private static int col2;
     
     //PRINT
     //Augmented from TwoDimArray.java
@@ -23,12 +34,14 @@ public class Swapper{
 	for (int Ctra = 0;
 	     a.length > Ctra;
 	     Ctra ++){
+	    System.out.print(Ctra + "\t");
 	    for (int Ctrb = 0;
 		 a[Ctra].length > Ctrb;
 		 //a[Ctra] instead of 0, covers arrays that aren't rectangles
 		 Ctrb ++){
-		System.out.print(a[Ctra][Ctrb] + " ");
+		System.out.print(a[Ctra][Ctrb] + "\t");
 	    }
+	    System.out.println();
 	    System.out.println();
 	    //makes new line after array at Ctra is printed before moving onto Ctra ++
 	}
@@ -40,14 +53,60 @@ public class Swapper{
 	//remember order matters with the equals
 	//spent a while trying to figure out why the Strings didn't swap correctly
 	//it was because I set the index to holder instead of the holder to index
-	holder = anArray[a][b];
-	anArray[a][b] = anArray[c][d];
-	anArray[c][d] = holder;
+	holder = anArray[row1][col1];
+	anArray[row1][col1] = anArray[row2][col2];
+	anArray[row2][col2] = holder;
     }
-    
+
+    //BEGIN
+    public static void swapping(String[][]array){
+	//Prompts
+	//wasn't sure where to put this so I referenced YoRPG.java
+	//for a better idea as to how to structure this
+	//Code is kind of repetitive and also using inst vars which I dislike
+	//Thought about making the input a String but that seemed
+	//more complicated to code
+
+	System.out.println("Note that you start counting rows and columns at 0 instead of 1");
+	System.out.println("Please input the location of the strings based on row and then column.\nExample: 0,1-2,3");
+	String input = Keyboard.readString();
+	row1 = Integer.parseInt(input.substring(0,1));
+	col1 = Integer.parseInt(input.substring(2,3));
+	row2 = Integer.parseInt(input.substring(4,5));
+	col2 = Integer.parseInt(input.substring(6,7));
+	
+	//Swap
+	swap(array);
+	
+	//Print array
+	System.out.println("\n\t0\t1\t2");
+	print1(array);
+    }
+
+    public static void continueSwapping(String[][]array){
+	//Prompts
+	System.out.println("Note that you start counting rows and columns at 0 instead of 1");
+	System.out.println("Please input the location of the strings based on row and then column.\nNote that you have a range of 0 to 3.\nExample: 0,1-2,3");
+	String input = Keyboard.readString();
+	row1 = Integer.parseInt(input.substring(0,1));
+	col1 = Integer.parseInt(input.substring(2,3));
+	row2 = Integer.parseInt(input.substring(4,5));
+	col2 = Integer.parseInt(input.substring(6,7));
+	
+	//Swap
+	swap(array);
+	
+	//Print array
+	System.out.println("\n\t0\t1\t2");
+	print1(array);
+
+	//Continue:
+	System.out.println("Continue Swapping? \n1 Yes \t 2 No");
+
+    }
+
     //MAIN METHOD
     public static void main(String [] args){
-	
 	//Create an Array
 	//I believe this is correct since every String is unique
 	//I don't understand the 3-4 char limitation though
@@ -59,35 +118,15 @@ public class Swapper{
 			      {"knif", "fork", "spon"} };//utensils
 	
 	//Print array
+	System.out.println("\n\t0\t1\t2");
 	print1(array);
-
-	//Prompts
-	//wasn't sure where to put this so I referenced YoRPG.java
-	//for a better idea as to how to structure this
-	//Code is kind of repetitive and also using inst vars which I dislike
-	//Thought about making the input a String but that seemed
-	//more complicated to code
-
-	System.out.println("Note that you start counting rows and columns at 0 instead of 1");
 	
-	System.out.println("Input row of first String to swap");
-	a = Keyboard.readInt();
-
-	System.out.println("Input column of first String to swap");
-	b = Keyboard.readInt();
-
-	System.out.println("Input row of second String to swap");
-	c = Keyboard.readInt();
-
-	System.out.println("Input column of second String to swap");
-	d = Keyboard.readInt();
-
-	//Swap
-	swap(array);
+	swapping(array);
+	System.out.println("Continue Swapping? \n1 Yes \t 2 No");
+	while(Keyboard.readInt() == 1){
+	    continueSwapping(array);
+	}
+	System.out.println("Exiting...");
 	
-	//Print array
-	print1(array);
-
-	//perhaps I can create a while loop that allows the user to choose whether or not to continue swapping?
     }
 }
