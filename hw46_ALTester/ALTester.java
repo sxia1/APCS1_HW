027371a22a2a30827b45e58da6ab766c9e770289
sxia1 <sxia1@stuy.edu>
2017-12-04 07:41:37 -0500

    hw46

---
+++ hw46_ALTester/ALTester.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+
+public class ALTester{
+    
+    public static void main(String [] args){
+	ArrayList shiboh = new ArrayList();
+	System.out.println(shiboh);// []
+	}
+    }//end main
+    
+}//end class ALTester
