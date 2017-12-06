b63f074e255a5e454bd0ecaeb8f9ab36ca30e916
sxia1 <sxia1@stuy.edu>
2017-12-06 07:41:45 -0500

    GUESS

---
+++ hw48_guessnumber/GuessNumber.java
@@ -0,0 +1,22 @@
+public class GuessNumber{
+    //Instance Variables
+    private static int num;
+    private int Ctr = 0;
+    private int upb = 100;
+    private int lowb = 1;
+
+    //methods
+    
+    //setNum
+    public static void setNum(){
+	num = (int)((Math.random()* 100) +1);
+    }
+    //Guessing...
+    public static void
+    
+    //main method
+    public static void main(String [] args){
+	
+    }//end main
+    
+}//end class GuessNumber
